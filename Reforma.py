ef0f97852a7ac0bb98c547ebb401d6afbbc5d743
Hugo Tomizawa <hstomizawa@hotmail.com>
2024-09-28 12:52:25 -0300

    Primeiro Commit

--- Reforma.py
+++ Reforma.py
@@ -18,13 +18,12 @@ print("*" * 30)
 if tamanhoReforma <= 0:
     print("Número inválido!")
 
-if tipoReforma == "Simples":
+if tipoReforma.upper() == "Simples":
     precoReforma = tamanhoReforma * 300
 elif tipoReforma == "Completa":
     precoReforma = tamanhoReforma * 500
 elif tipoReforma == "Luxo":
     precoReforma = tamanhoReforma * 800
-
 
 if tipoMaterial == "Padrão":
     descontoMaterial = 0
@@ -33,14 +32,12 @@ elif tipoMaterial == "Premium":
 elif tipoMaterial == "Luxo":
     descontoMaterial = precoReforma * 0.5
 
-
 if tamanhoReforma > 100:
     descontoTamanho = precoReforma * 0.1
 elif tamanhoReforma < 30:
     descontoTamanho = - precoReforma * 1.15
 else:
     descontoTamanho = 0
-
 
 if tamanhoReforma > 150:
     precoEquipe = 2000 * (math.ceil((tamanhoReforma - 150) / 50))
@@ -49,7 +46,7 @@ else:
 
 if localizacao == "Central":
     precoFinal = precoReforma + descontoMaterial - (descontoTamanho) + precoEquipe + 5000
-    print(f"O preço final da reforma é: R${precoFinal:.2f}")
 elif localizacao == "Periférica":
     precoFinal = precoReforma + descontoMaterial - (descontoTamanho) + precoEquipe + 3000
-    print(f"O preço final da reforma é: R${precoFinal:.2f}")
+
+print(f"O preço final da reforma é: R${precoFinal:.2f}")
