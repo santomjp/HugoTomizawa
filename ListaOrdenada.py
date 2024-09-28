9916cfe94b355f6f275ecd47123213d38e9f831c
Hugo Tomizawa <hstomizawa@hotmail.com>
2024-09-28 17:51:20 -0300

    Alteração

--- ListaOrdenada.py
+++ ListaOrdenada.py
@@ -8,10 +8,12 @@ while naoContinua != 0:
 
 ordem = input("Deseja organizar a lista em ordem crescente ou decrescente?\n")
 
-if ordem.upper() == "crescente":
+if ordem.upper() == "CRESCENTE":
     for i in range(len(lista)):
         while lista[i] >= lista[i + 1]:
             c = lista[i]
             lista[i] = lista[i + 1]
             lista[i + 1] = c
-        print()
+
+
+print(lista)
